beef8599f1d27e9f59dcd0d68c5ea410c8591699
Arthur Pavão <apavao@gazeus.com>
2018-02-23 18:23:52 -0300

    still setting up server

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,13 @@
 var express = require('express');
 var app = express();
 
+// routes
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send('hello world!');
 });
+
+// static files
+app.use(express.static(__dirname + '/www'));
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
